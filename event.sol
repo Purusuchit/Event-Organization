560d3c6d2de91f9cb98b0b609d6a6d21f767d827
Purusuchit <suchit007lko@gmail.com>
2022-11-20 03:08:23 +0530

    commit

---
+++ event.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract EventContract {
+ struct Event {
+     address organizer;
+     string name;
+     uint date;
+     uint price;
+     uint ticketCount;
+     uint ticketRemain;
+ }
+
+ mapping(uint => Event) public events;
+ mapping(address => mapping(uint=>uint)) public tickets;
+ uint public nextId;
+
+ function createEvent(string memory name, uint date, uint price, uint ticketCount) external{
+     
+   require(date>block.timestamp,"You can organize event for future date");
+   require(ticketCount>0,"You can organize event only if you create more than 0 tickets");
+
+   events[nextId] = Event(msg.sender,name,date,price,ticketCount,ticketCount);
+   nextId++;  
+ }
+
+ function buyTicket(uint id,uint quantity) external payable{
+
+     require(events[id].date!=0,"This event does not exixts");
+     require(events[id].date > block.timestamp,"Event has already occured"); 
+     Event storage _event = events[id];
+     require(msg.value==(_event.price*quantity), "Ether is not enough");
+     require(_event.ticketRemain>=quantity,"Not enough tickets remain");
+     _event.ticketRemain-=quantity;
+     tickets[msg.sender][id]+=quantity;
+
+ }
+
+ function transferTicket(uint id, uint quantity,address to) external{
+
+     require(events[id].date!=0,"This event does not exixts");
+     require(events[id].date > block.timestamp,"Event has already occured"); 
+     require(tickets[msg.sender][id]>=quantity,"You don't have enough tickets ");
+     tickets[msg.sender][id]-=quantity;
+     tickets[to][id]+=quantity;
+ }
+
+}
